3574aa1e6b22c1d3259b80619166832ad99c5f3d
Max Raphael <mrap96@gmail.com>
2021-05-12 17:24:47 -0400

    implement table

--- backpack/migrations/20210512212228_create_categories_articles_table.js
+++ backpack/migrations/20210512212228_create_categories_articles_table.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex) {
+    return knex.schema.createTable('categories_articles', table => {
+        table.integer('category_id').notNullable()
+        table.foreign('category_id').references('category_id').onDelete('CASCADE')
+        table.integer('article_id').notNullable()
+        table.foreign('article_id').references('article_id').onDelete('CASCADE')
+        table.timestamps(true, true)
+    })
+  };
   
-};
-
-exports.down = function(knex) {
+  exports.down = function(knex) {
+    knex.schema.dropTable('categories_articles')
+  };
   
-};
