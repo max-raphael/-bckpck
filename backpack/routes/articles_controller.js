bff8aea97ef63eecec7ab1c0d640f70239786f58
Max Raphael <mrap96@gmail.com>
2021-05-13 17:07:58 -0400

    outline articles controller

--- backpack/routes/articles_controller.js
+++ backpack/routes/articles_controller.js
@@ -1,7 +1,17 @@
+// Set up required files and configurations
 let express = require('express');
 let router = express.Router();
 require('dotenv').config()
 let knex = require('../models/db')
 
+// Get all articles, categories, and websites to render articles page and dropdown filter menu
+
+// Get articles by category and/or website
+
+// Post article
+
+// Update category or title of article with id == req.params.id
+
+// Delete article
 
 module.exports = router;
