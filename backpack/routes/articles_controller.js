50b95e29dbc72e7a062b4b869ba2c861f43a9870
Max Raphael <mrap96@gmail.com>
2021-05-13 17:36:23 -0400

    get route working properly

--- backpack/routes/articles_controller.js
+++ backpack/routes/articles_controller.js
@@ -5,6 +5,25 @@ require('dotenv').config()
 let knex = require('../models/db')
 
 // Get all articles, categories, and websites to render articles page and dropdown filter menu
+router.get('/', (req, res) => {
+    knex('articles')
+        .then(articles => {
+            knex('articles')
+                // Get categories for articles that currently exist and exclude those that have been deleted from populating the dropdown menus
+                .join('categories_articles', 'articles.id', 'categories_articles.article_id')
+                .join('categories', 'categories.id', 'categories_articles.category_id')
+                .distinct('category')
+                .then(categories => {
+                    knex('articles')
+                        // Get websites for articles that currently exist and exclude those that have been deleted from populating the dropdown menus
+                        .join('websites', 'articles.website_id', 'websites.id')
+                        .distinct('website')
+                        .then(websites => {
+                            res.json(articles, categories, websites)
+                        })
+                })
+        })
+})
 
 // Get articles by category and/or website
 
